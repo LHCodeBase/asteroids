245b81697e62ff08ca964eb998b550cc4596cfab
Kendrick Leiter <lhcodebase@gmail.com>
2025-02-06 19:58:38 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    main()
