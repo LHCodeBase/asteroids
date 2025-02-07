ebc775200aa7eea88fbc867274cd00895c88086a
Kendrick Leiter <lhcodebase@gmail.com>
2025-02-06 22:51:08 -0500

    Add screen and ensure it closes on mac

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import pygame
 from constants import *
 
 def main():
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    keepGoing = True
+    # Set initial screen size
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    while keepGoing:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill('black')
+        pygame.display.flip()
     pass
 
 if __name__ == "__main__":
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     main()
