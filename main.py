f420d132dc26b9704ef8c2726392e07a42c04380
Kendrick Leiter <lhcodebase@gmail.com>
2025-02-07 13:56:42 -0500

    Add player sprite with rotation

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    keepGoing = True
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     # Set initial screen size
+    keepGoing = True
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while keepGoing:
+        # Exit if user clicks X on window (Needed for mac)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        screen.fill('black')
-        pygame.display.flip()
+                keepGoing = False
+                return # breaks out of parent function
+
+        # Decouple FPS from CPU clock
+        dt = (clock.tick(60) / 1000) # pause 1/60th of a second
+
+        # Background
+        screen.fill('black') # NOTE: rgb should be 255, 0, 0 or '#ffffff'
+        player.draw(screen)
+        player.update(dt)
+
+        pygame.display.flip() # Render screen
+
+    pygame.quit()
     pass
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,39 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+
+class Player(CircleShape):
+    def __init__(self, x: int, y: int):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        pass
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * \
+                self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        color = (255, 255, 255)
+        pygame.draw.polygon(screen, color, self.triangle(), width=2)
+        pass
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        pass
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            # rotate "left" counter-clockwise
+            self.rotate(dt * -1)
+
+        if keys[pygame.K_d or keys[pygame.K_RIGHT]]:
+            # rotate "right" clockwise
+            self.rotate(dt)
+        pass
